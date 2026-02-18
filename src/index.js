5159f162ae165b9c9f14fc4db5732c922115d60e
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-02-18 16:47:28 -0300

    feat: add PUT, PATCH and DELETE routes for pets

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import { randomUUID } from "crypto";
 import * as dotenv from "dotenv";
 import { pets } from "./pets.js";
+import { validateFieldsPost } from "./middlewares.js";
+import { validateFieldsPut } from "./middlewares.js";
 const app = express();
 app.use(express());
 app.use(express.json())
@@ -38,58 +40,30 @@ app.get("/pets", (req, res) => {
     }
 })
 
+app.post("/pets", [validateFieldsPost], (req, res) => {
+  try {
+    const body = req.body;
+    const newPet = {
+      id: randomUUID(),
+      name: body.name,
+      breed: body.breed,
+      age: body.age,
+      ownerName: body.ownerName,
+    };
+    pets.push(newPet);
 
-app.post("/pets", (req, res) => {
-    try{
-        const body = req.body;
-        if (!body.name) {
-            return res.status(400).send({
-            ok: false,
-            message: "O nome do pet precisa ser informado",
-            });
-        }
-        if (!body.breed) {
-            return res.status(400).send({
-            ok: false,
-            message: "A raça do pet deve ser informada",
-            });
-        }
-        if (!body.age) {
-            return res.status(400).send({
-            ok: false,
-            message: "A idade do pet precisa ser informada",
-            });
-        }
-        if (!body.ownerName) {
-            return res.status(400).send({
-            ok: false,
-            message: "O nome do tutor precisa ser informado",
-            });
-        }
-
-        const newPet = {
-            id: randomUUID(),
-            name: body.name,
-            breed: body.breed,
-            age: body.age,
-            ownerName: body.ownerName,
-        };
-        pets.push(newPet);
-        
-        res.status(201).send({
-            ok: true,
-            message: "Novo pet criado com sucesso",
-            newPet,
-        });
-
-    }catch(error){
-        res.status(500).send({
-          ok: false,
-          message: `Erro no servidor ${error.toString()}`,
-        });
-    }
-
-})
+    res.status(201).send({
+      ok: true,
+      message: "Novo pet criado com sucesso",
+      newPet,
+    });
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      message: `Erro no servidor ${error.toString()}`,
+    });
+  }
+});
 
 app.get("/pets/:id", (req, res) => {
     try{
@@ -114,6 +88,82 @@ app.get("/pets/:id", (req, res) => {
     }
 
 })
+
+app.put("/pets/:id", [validateFieldsPut], (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, breed, age, ownerName } = req.body;
+    const pet = pets.find((item) => item.id === id);
+    pet.name = name;
+    pet.breed = breed;
+    pet.age = age;
+    pet.ownerName = ownerName;
+
+    res.status(200).send({
+      ok: true,
+      message: "Pet atualizado com sucesso",
+      pet,
+      data: pets,
+    });
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      message: `Erro no servidor ${error.toString()}`,
+    });
+  }
+});
+
+app.patch("/pets/:id", (req, res) => {
+    try{
+        const { id } = req.params;
+        const pet = pets.find((item) => item.id === id);
+        if (!pet) {
+            return res.status(404).send({
+            ok: false,
+            message: "Pet nao encontrado",
+            });
+        }
+
+        pet.registration = !pet.registration;
+        res.status(200).send({
+            ok: true,
+            message: "Matricula alterada com sucesso",
+        });
+    }catch(error){
+        res.status(500).send({
+            ok: false,
+            message: `Erro no servidor ${error.toString()}`
+        })
+    }
+})
+
+app.delete("/pets/:id", (req, res) => {
+    try{
+        const { id } = req.params;
+        const pet = pets.find((item) => item.id === id);
+        if (!pet) {
+            return res.status(404).send({
+            ok: false,
+            message: "Pet nao encontrado, verifique e tente novamente",
+            });
+        }
+
+        const petIndex = pets.indexOf((item) => item.id === id)
+        pets.splice(petIndex, 1);
+        res.status(200).send({
+            ok: true,
+            message: "Pet deletado com sucesso da lista",
+            dataPets: pets
+        })
+
+    }catch(error){
+        res.status(500),send({
+            ok: false,
+            message: `Erro no servidor ${error.toString()}`
+        })
+    }
+})  
+
 const port = process.env.PORT
 app.listen(port, () => {
     console.log(`servidor rodando na porta ${port}`)
