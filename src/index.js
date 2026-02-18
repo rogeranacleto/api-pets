38dd9bca4501e80fc3472ff334cd06cbba7cec02
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-02-17 23:26:14 -0300

    feat: add POST /pets and GET /pets/:id endpoints with validations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
+import { randomUUID } from "crypto";
 import * as dotenv from "dotenv";
-import { pets } from "./pets";
+import { pets } from "./pets.js";
 const app = express();
 app.use(express());
 app.use(express.json())
@@ -38,24 +39,81 @@ app.get("/pets", (req, res) => {
 })
 
 
+app.post("/pets", (req, res) => {
+    try{
+        const body = req.body;
+        if (!body.name) {
+            return res.status(400).send({
+            ok: false,
+            message: "O nome do pet precisa ser informado",
+            });
+        }
+        if (!body.breed) {
+            return res.status(400).send({
+            ok: false,
+            message: "A raça do pet deve ser informada",
+            });
+        }
+        if (!body.age) {
+            return res.status(400).send({
+            ok: false,
+            message: "A idade do pet precisa ser informada",
+            });
+        }
+        if (!body.ownerName) {
+            return res.status(400).send({
+            ok: false,
+            message: "O nome do tutor precisa ser informado",
+            });
+        }
 
+        const newPet = {
+            id: randomUUID(),
+            name: body.name,
+            breed: body.breed,
+            age: body.age,
+            ownerName: body.ownerName,
+        };
+        pets.push(newPet);
+        
+        res.status(201).send({
+            ok: true,
+            message: "Novo pet criado com sucesso",
+            newPet,
+        });
 
+    }catch(error){
+        res.status(500).send({
+          ok: false,
+          message: `Erro no servidor ${error.toString()}`,
+        });
+    }
 
+})
 
+app.get("/pets/:id", (req, res) => {
+    try{
+        const { id } = req.params;
+        const getPet = pets.find((item) => item.id === id);
+        if(!getPet){
+            res.status(404).send({
+                ok: false,
+                message: "Pet nao encontrado, verifique e tente novamente"
+            })
+        }
+        res.status(200).send({
+            ok: true,
+            message: "Pet encontrado com sucesso",
+            petData: getPet
+        })
+    }catch(error){
+        res.status(500).send({
+            ok: false,
+            message: `Erro no servidor ${error.toString()}`
+        })
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
 const port = process.env.PORT
 app.listen(port, () => {
     console.log(`servidor rodando na porta ${port}`)
